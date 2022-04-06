91a3a7074537fa6819c69ed88563a35655ac2de4
Jacob Logan <jacoblogan98@gmail.com>
2022-04-06 12:27:22 -0400

    commit tree 61e7ae870910664e83968a38a985a98d1bbdda6c

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "./Article"
+
+function ArticleList({posts}) {
+    const renderArticle = posts.map(post => {
+        return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+    });
+
+    return (
+        <main>
+            {renderArticle}
+        </main>
+    );
+}
+
+export default ArticleList;
